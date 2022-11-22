e350cf755cf15031c28a6a0e82fe214f6980f95c
nnnnadia <tristaonadia@gmail.com>
2022-11-22 19:08:18 -0300

    Requisito 5: <class> `Mage`: static attribute and method for counting class intances

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -2,7 +2,16 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Mage extends Archetype {
+  static archetypeInstances = 0;
+
   private _energyType: EnergyType = 'mana';
 
+  constructor(name: string) {
+    super(name);
+    Mage.archetypeInstances += 1;
+  }
+
   get energyType(): EnergyType { return this._energyType; }
+
+  static createdArchetypeInstances(): number { return Mage.archetypeInstances; }
 }
