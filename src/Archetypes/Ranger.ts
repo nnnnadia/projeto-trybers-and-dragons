e1383492c6cc7bf971ebabcac51f21a695e4a055
nnnnadia <tristaonadia@gmail.com>
2022-11-22 18:52:50 -0300

    Requisito 5: <class> `Ranger`: start private attribute and set getter (energyType)

--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -1,5 +1,8 @@
+import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Ranger extends Archetype {
-  
+  private _energyType: EnergyType = 'stamina';
+
+  get energyType(): EnergyType { return this._energyType; }
 }
