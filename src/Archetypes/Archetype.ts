850e0b3923b38ab6d36ff03de1c920ae9072fd79
nnnnadia <tristaonadia@gmail.com>
2022-11-22 16:53:07 -0300

    Requisito 4: <class> `Archetype`: constructor e getters for private attributes (name, special, cost)

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,3 +1,11 @@
 export default abstract class Archetype {
+  constructor(
+    private _name: string,
+    private _special = 0,
+    private _cost = 0,
+  ) { }
 
+  get name(): string { return this._name; }
+  get special(): number { return this._special; }
+  get cost(): number { return this._cost; }
 }
