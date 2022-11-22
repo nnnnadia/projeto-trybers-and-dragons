a66e789eb20f3f43773c7087eb22cc01847e4645
nnnnadia <tristaonadia@gmail.com>
2022-11-22 16:44:51 -0300

    Requisito 2: <class> `Halfling`: static attribute and method for counting class intances

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,7 +1,13 @@
 import Race from './Race';
 
 export default class Halfling extends Race {
+  static raceInstances = 0;
   private _maxLifePoints = 60;
 
   get maxLifePoints(): number { return this._maxLifePoints; }
+
+  static createdRacesInstances(): number {
+    Halfling.raceInstances += 1;
+    return Halfling.raceInstances;
+  }
 }
