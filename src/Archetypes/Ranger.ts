15203eaa55e8be58c408006438226a12fb8ba887
nnnnadia <tristaonadia@gmail.com>
2022-11-22 18:54:42 -0300

    Requisito 5: <class> `Ranger`: static attribute and method for counting class intances

--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -2,7 +2,18 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Ranger extends Archetype {
+  static archetypeInstances = 0;
+
   private _energyType: EnergyType = 'stamina';
 
+  constructor(name: string, special: number, cost: number) {
+    super(name, special, cost);
+    Ranger.archetypeInstances += 1;
+  }
+
   get energyType(): EnergyType { return this._energyType; }
+
+  static createdArchetypeInstances(): number {
+    return Ranger.archetypeInstances;
+  }
 }
