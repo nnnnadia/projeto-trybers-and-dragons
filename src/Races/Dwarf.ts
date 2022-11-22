bfd0b0bae813183094c0153e5440b306389108c6
nnnnadia <tristaonadia@gmail.com>
2022-11-22 19:11:16 -0300

    refactor: fix Dwarf race instances count

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -2,13 +2,16 @@ import Race from './Race';
 
 export default class Dwarf extends Race {
   static raceInstances = 0;
-
   private _maxLifePoints = 80;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Dwarf.raceInstances += 1;
+  }
 
   get maxLifePoints(): number { return this._maxLifePoints; }
 
   static createdRacesInstances(): number {
-    Dwarf.raceInstances += 1;
     return Dwarf.raceInstances;
   }
 }
