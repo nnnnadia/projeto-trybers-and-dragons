d62de7e1e47f960731c60c5de8f277fddd753c51
nnnnadia <tristaonadia@gmail.com>
2022-11-22 16:44:39 -0300

    Requisito 2: <class> `Elf`: static attribute and method for counting class intances

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -1,7 +1,13 @@
 import Race from './Race';
 
 export default class Elf extends Race {
+  static raceInstances = 0;
   private _maxLifePoints = 99;
 
   get maxLifePoints(): number { return this._maxLifePoints; }
+
+  static createdRacesInstances(): number {
+    Elf.raceInstances += 1;
+    return Elf.raceInstances;
+  }
 }
