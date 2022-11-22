39519f689a34cc123dc01dd995e5c5d5ac1d6807
nnnnadia <tristaonadia@gmail.com>
2022-11-22 18:50:23 -0300

    Requisito 5: <class> `Mage`: start private attribute and set getter (energyType)

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -1,5 +1,8 @@
+import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Mage extends Archetype {
-  
+  private _energyType: EnergyType = 'mana';
+
+  get energyType(): EnergyType { return this._energyType; }
 }
