c72351d9285bb7fadfdd43b96959e5cd6d888826
nnnnadia <tristaonadia@gmail.com>
2022-11-22 16:43:44 -0300

    Requisito 2: <class> `Dwarf`: static attribute and method for counting class intances

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -1,7 +1,14 @@
 import Race from './Race';
 
 export default class Dwarf extends Race {
+  static raceInstances = 0;
+
   private _maxLifePoints = 80;
 
   get maxLifePoints(): number { return this._maxLifePoints; }
+
+  static createdRacesInstances(): number {
+    Dwarf.raceInstances += 1;
+    return Dwarf.raceInstances;
+  }
 }
