f646830989126258de5fbd507c8e7b50e624cd09
nnnnadia <tristaonadia@gmail.com>
2022-11-22 19:09:08 -0300

    Requisito 5: <class> `Necromancer`: static attribute and method for counting class intances

--- src/Archetypes/Necromancer.ts
+++ src/Archetypes/Necromancer.ts
@@ -2,7 +2,18 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Necromancer extends Archetype {
+  static archetypeInstances = 0;
+
   private _energyType: EnergyType = 'mana';
 
+  constructor(name: string) {
+    super(name);
+    Necromancer.archetypeInstances += 1;
+  }
+
   get energyType(): EnergyType { return this._energyType; }
+
+  static createdArchetypeInstances(): number {
+    return Necromancer.archetypeInstances;
+  }
 }
