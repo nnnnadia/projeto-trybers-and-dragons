150804416576eb4f049170e0b5096e0ad0307c86
nnnnadia <tristaonadia@gmail.com>
2022-11-22 16:19:36 -0300

    Requisito 1: <class> `Race`: constructor e getters for private attributes (name, dexterity)

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,3 +1,6 @@
 export default abstract class Race {
+  constructor(private _name: string, private _dexterity: number) { }
 
+  get name() { return this._name; }
+  get dexterity() { return this._dexterity; }
 }
