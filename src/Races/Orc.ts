ab883659ad1605cd1e7a19573cf3c15cf5a7cf3d
nnnnadia <tristaonadia@gmail.com>
2022-11-22 16:45:04 -0300

    Requisito 2: <class> `Orc`: static attribute and method for counting class intances

--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -1,7 +1,13 @@
 import Race from './Race';
 
 export default class Orc extends Race {
+  static raceInstances = 0;
   private _maxLifePoints = 74;
 
   get maxLifePoints(): number { return this._maxLifePoints; }
+
+  static createdRacesInstances(): number {
+    Orc.raceInstances += 1;
+    return Orc.raceInstances;
+  }
 }
