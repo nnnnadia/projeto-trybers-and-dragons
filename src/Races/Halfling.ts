93bf5ce90693230ef30a4297511a5899f5dae93f
nnnnadia <tristaonadia@gmail.com>
2022-11-22 19:11:55 -0300

    refactor: fix Halfling race instances count

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -4,10 +4,14 @@ export default class Halfling extends Race {
   static raceInstances = 0;
   private _maxLifePoints = 60;
 
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Halfling.raceInstances += 1;
+  }
+
   get maxLifePoints(): number { return this._maxLifePoints; }
 
   static createdRacesInstances(): number {
-    Halfling.raceInstances += 1;
     return Halfling.raceInstances;
   }
 }
