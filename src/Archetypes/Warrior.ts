1f405f5d9eba4efdb05469c7763662a08246cf60
nnnnadia <tristaonadia@gmail.com>
2022-11-22 19:09:37 -0300

    Requisito 5: <class> `Warrior`: static attribute and method for counting class intances

--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -2,7 +2,18 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Warrior extends Archetype {
+  static archetypeInstances = 0;
+
   private _energyType: EnergyType = 'stamina';
 
+  constructor(name: string) {
+    super(name);
+    Warrior.archetypeInstances += 1;
+  }
+
   get energyType(): EnergyType { return this._energyType; }
+
+  static createdArchetypeInstances(): number {
+    return Warrior.archetypeInstances;
+  }
 }
